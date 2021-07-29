fe4949633d575ac072b2a889e1f047ca7c4e544c
johnson <poshenc@gmail.com>
2021-07-29 14:59:56 +0800

    add printing sever log function

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
-// 設定首頁路由
+app.use(print)
+
 app.get('/', (req, res) => {
-  res.send('hello world-')
+  res.send('列出全部 Todo <br> <a href="/new">新增 Todo 頁面</a> <br> <a href="/:id">顯示一筆 Todo</a> <br> <form action="/" method="POST"> <button type="submit">Create</button></form >')
 })
 
-// 設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
 })
+
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})
+
+function print(req, res, next) {
+  const currentTime = new Date().toLocaleString()
+  console.log(currentTime, '|', req.method, 'from', req.originalUrl)
+  next()
+}
